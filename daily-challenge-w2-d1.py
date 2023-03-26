ea8a7e093826bd81a36d35546a7aacbeafd50f9f
Weber0906 <filippovno@icloud.com>
2023-03-26 16:25:07 +0300

    Added XP exercise W2 D1

--- daily-challenge-w2-d1.py
+++ daily-challenge-w2-d1.py
@@ -1,3 +1,10 @@
-def print_loop(Hello):
-    for i in range(1, len(Hello) + 1):
-    print(Hello[0:i])
+# Using the input function, ask the user for a string. The string must be 10 characters long.
+# If it’s less than 10 characters, print a message which states “string not long enough”.
+# If it’s more than 10 characters, print a message which states “string too long”.
+
+
+# Then, print the first and last characters of the given text.
+
+# Using a for loop, construct the string character by character: Print the first character, then the second, then the third, until the full string is printed.
+
+# Bonus: Swap some characters around then print the newly jumbled string (hint: look into the shuffle method).
