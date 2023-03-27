77af7da0a1931c23fbaec976ee386db6f6d1c22f
Weber0906 <filippovno@icloud.com>
2023-03-27 10:13:24 +0300

    Added daily challenge w2 d1

--- daily-challenge-w2-d1.py
+++ daily-challenge-w2-d1.py
@@ -2,9 +2,28 @@
 # If it’s less than 10 characters, print a message which states “string not long enough”.
 # If it’s more than 10 characters, print a message which states “string too long”.
 
+from random import shuffle
+
+
+string = input("At least 10 characters:")
+if len(string) < 10:
+    print("String not long enough")
+else:
+    print("String too long")
+
 
 # Then, print the first and last characters of the given text.
 
-# Using a for loop, construct the string character by character: Print the first character, then the second, then the third, until the full string is printed.
+print(string[0:len(string):len(string)-1])
 
+# Using a for loop, construct the string character by character: Print the first character, then the second, then the third, until the full string is printed.
+c=1
+for i in string:
+    print(string[0:c])
+    c+=1
 # Bonus: Swap some characters around then print the newly jumbled string (hint: look into the shuffle method).
+s = list(string)
+s_list = list(s)
+shuffle(s_list)
+s = "".join(s_list)
+print(s)
