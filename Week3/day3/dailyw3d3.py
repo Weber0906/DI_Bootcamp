e486c5b2bfd7035e973e28f5b2d126d2efafd076
Weber0906 <filippovno@icloud.com>
2023-04-16 01:59:51 +0300

    added both daily challenges w3d3

--- Week3/day3/dailyw3d3.py
+++ Week3/day3/dailyw3d3.py
@@ -12,48 +12,93 @@
 # Be able to compare two circles to see which is bigger
 # Be able to compare two circles and see if there are equal
 # Be able to put them in a list and sort them
-from math import pi
+from translate import Translator
+import math
 
 
 class Circle:
+    def __init__(self, radius=None, diameter=None):
+        if radius is not None:
+            self.radius = radius
+        elif diameter is not None:
+            self.radius = diameter / 2
+        else:
+            raise ValueError("Must specify either radius or diameter")
 
-    def __init__(self, radius: float) -> None:
-        self.radius = radius
-        self.diameter = radius * 2
+    @property
+    def diameter(self):
+        return self.radius * 2
 
-    @classmethod
-    def from_diameter(cls, diameter: float):
-        new_circle = cls(diameter / 2)
-        return new_circle
+    @diameter.setter
+    def diameter(self, value):
+        self.radius = value / 2
 
     @property
     def area(self):
-        area = pi * self.radius ** 2
-        return area
+        return math.pi * self.radius ** 2
 
-    def __add__(self, other_circle):
-        radius_combined = self.radius + other_circle.radius
-        new_circle = Circle(radius_combined)
-        return new_circle
+    def __str__(self):
+        return f"Circle with radius {self.radius:.2f}"
 
-    def __lt__(self, other_circle):
-        return self.radius < other_circle.radius
+    def __repr__(self):
+        return f"Circle({self.radius})"
+
+    def __add__(self, other):
+        return Circle(self.radius + other.radius)
+
+    def __lt__(self, other):
+        return self.radius < other.radius
+
+    def __eq__(self, other):
+        return self.radius == other.radius
+
+    def __gt__(self, other):
+        return self.radius > other.radius
 
 
-c1 = Circle(2.0)
+c1 = Circle(radius=3)
+print(c1.radius)
+print(c1.diameter)
+print(c1.area)
+print(str(c1))
+print(repr(c1))
 
-c2 = Circle.from_diameter(6.0)
-
-
-print("Radius:", c1.radius)
-print("Diameter circle1", c1.diameter)
-
-print("Radius:", c2.radius)
-print("Diameter circle1", c2.diameter)
-
-print("Area", c1.area)
-print("Area", c2.area)
+c2 = Circle(diameter=4)
+print(c2.radius)
+print(c2.diameter)
+print(c2.area)
+print(str(c2))
+print(repr(c2))
 
 c3 = c1 + c2
+print(c3.radius)
+print(c3.diameter)
+print(c3.area)
+print(str(c3))
+print(repr(c3))
 
-print(c3)
+print(c2 < c1)
+print(c2 == Circle(radius=2))
+
+circles = [c3, c2, c1]
+print(sorted(circles))
+
+
+# Consider this list
+
+# french_words= ["Bonjour", "Au revoir", "Bienvenue", "A bientôt"]
+# Look at this result :
+# {"Bonjour": "Hello", "Au revoir": "Goodbye", "Bienvenue": "Welcome", "A bientôt": "See you soon"}
+# You have to recreate the result using a translator module.
+
+
+french_words = ["Bonjour", "Au revoir", "Bienvenue", "A bientôt"]
+
+
+translator = Translator(from_lang="fr", to_lang="en")
+
+
+translated_words = {word: translator.translate(word) for word in french_words}
+
+
+print(translated_words)
