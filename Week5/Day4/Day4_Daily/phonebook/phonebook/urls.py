ff8f579232fd7ce69738646accc0dea02a8d4c39
Weber0906 <filippovno@icloud.com>
2023-05-15 22:10:45 +0300

    added week5 day 4 daily

---
+++ Week5/Day4/Day4_Daily/phonebook/phonebook/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from phone_app.views import PhoneNumberView, NameView, SearchView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('persons/phonenumber/<str:phone_number>/', PhoneNumberView.as_view(), name='phone_number_view'),
+    path('persons/name/<str:name>/', NameView.as_view(), name='name_view'),
+    path('search/', SearchView.as_view(), name='search_view'),
+]
